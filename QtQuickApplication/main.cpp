16d26616acc063b5bf7938de6fc9d79e11176f6a
D067836 <Lars.Huebner@sap.com>
2017-11-01 09:47:40 +0100

    Update Main-function
    - change "return 0" to "retrun EXIT_FAILURE" from <cstdlib>

--- QtQuickApplication/main.cpp
+++ QtQuickApplication/main.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
@@ -23,7 +24,7 @@ int main(int argc, char *argv[])
      */
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())
-        return -1;
+        return EXIT_FAILURE;
 
     return app.exec();
 }
