070a873595e61eaf8b01be717adb7f2c8d9900b0
Christopher <christopher.klammt@gmail.com>
2018-05-03 09:17:13 +0200

    AppVeyor

--- VisualRacing/VRMain/VRMain.pro
+++ VisualRacing/VRMain/VRMain.pro
@@ -1,6 +1,7 @@
 QT += quick printsupport
 #CONFIG += c++11
-QMAKE_CXXFLAGS += -std=c++1y
+QMAKE_CXXFLAGS += -std=c++14 \
+    -std=gnu++14
 
 # The following define makes your compiler emit warnings if you use
 # any feature of Qt which as been marked deprecated (the exact warnings
--- appveyor/build_windows.cmd
+++ appveyor/build_windows.cmd
@@ -3,20 +3,17 @@ echo on
 SET project_dir="%cd%"
 SET PATH=%QT%\bin\;C:\Qt\Tools\QtCreator\bin\;C:\Qt\QtIFW2.0.1\bin\;%PATH%
 
-echo Set up environment...
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %PLATFORM%
-
 cd VisualRacing 
 
 echo Building YourApp using ming232-make...
 qmake VisualRacing.pro -spec win32-g++ "CONFIG+=debug" "CONFIG+=qml_debug" 
 mingw32-make
 
+echo Set up environment...
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %PLATFORM%
+
 echo Building YourApp using nmake...
 qmake -spec win32-msvc2015 CONFIG+=x86_64 CONFIG-=debug CONFIG+=release
 nmake
-
-echo Running tests...
-dir
 
 
