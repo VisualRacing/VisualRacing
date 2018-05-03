b9b84dc32a0fb7fd993f5ebe5b1d10a448d4f681
Christopher <christopher.klammt@gmail.com>
2018-05-03 09:30:28 +0200

    mingw

--- appveyor/build_windows.cmd
+++ appveyor/build_windows.cmd
@@ -1,7 +1,7 @@
 echo on
 
 SET project_dir="%cd%"
-SET PATH=%QT%\bin\;C:\Qt\Tools\QtCreator\bin\;C:\Qt\QtIFW2.0.1\bin\;%PATH%
+SET PATH=%QT%\bin\;C:\Qt\Tools\QtCreator\bin\;C:\Qt\QtIFW2.0.1\bin\;C:\MinGW\bin;%PATH%
 
 cd VisualRacing 
 
