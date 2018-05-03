595564d8a59c8209d9bd0d2a0ffd6fe1110e00cb
Christopher <christopher.klammt@gmail.com>
2018-05-03 08:32:32 +0200

    Fix

--- appveyor/build_windows.cmd
+++ appveyor/build_windows.cmd
@@ -5,8 +5,8 @@ SET project_dir="%cd%"
 echo Set up environment...
 
 echo Building YourApp...
-cd VRMain\ 
-if not exist Makefile qmake -o Makefile VRMain.pro -spec win32-g++ "CONFIG+=debug" "CONFIG+=qml_debug" 
+cd VisualRacing 
+if not exist Makefile qmake -o Makefile VisualRacing.pro -spec win32-g++ "CONFIG+=debug" "CONFIG+=qml_debug" 
 mingw32-make -f Makefile
 
 echo Running tests...
