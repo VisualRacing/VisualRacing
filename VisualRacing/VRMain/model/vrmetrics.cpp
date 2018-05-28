065036e5c63dd43f2b86502bd0dc5ce7f2f3d2b0
huebnerl <LarsHuebner2@web.de>
2018-05-28 23:05:19 +0200

    Added shift Time value.

--- VisualRacing/VRMain/model/vrmetrics.cpp
+++ VisualRacing/VRMain/model/vrmetrics.cpp
@@ -4,6 +4,7 @@ VRMetrics::VRMetrics()
 {
     this->accelBehav = 0;
     this->avgAccelBehav = 0;
+    this->shiftTime = 0;
 }
 
 VRMetrics::~VRMetrics()
@@ -31,4 +32,15 @@ void VRMetrics::setAvgAccelBehav(float value)
 {
     this->avgAccelBehav = value;
     emit this->avgAccelBehavChanged();
+}
+
+float VRMetrics::getShiftTime() const
+{
+    return this->shiftTime;
+}
+
+void VRMetrics::setShiftTime(float value)
+{
+    this->shiftTime = value;
+    emit this->shiftTimeChanged();
 }
--- VisualRacing/VRMain/model/vrmetrics.h
+++ VisualRacing/VRMain/model/vrmetrics.h
@@ -8,6 +8,7 @@ class VRMetrics : public QObject
     Q_OBJECT
     Q_PROPERTY(float accelBehav READ getAccelBehav WRITE setAccelBehav NOTIFY accelBehavChanged)
     Q_PROPERTY(float avgAccelBehav READ avgAccelBehav WRITE setAvgAccelBehav NOTIFY avgAccelBehavChanged)
+    Q_PROPERTY(float shiftTime READ getShiftTime WRITE setShiftTime NOTIFY shiftTimeChanged)
 
 public:
     VRMetrics();
@@ -19,15 +20,19 @@ public:
     float getAvgAccelBehav() const;
     void setAvgAccelBehav(float value);
 
+    float getShiftTime() const;
+    void setShiftTime(float value);
+
 signals:
     void accelBehavChanged();
     void avgAccelBehavChanged();
+    void shiftTimeChanged();
 
 private:
 
     float accelBehav;
     float avgAccelBehav;
-
+    float shiftTime;
 };
 
 #endif // VRMETRICS_H
