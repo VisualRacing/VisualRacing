47a2235f503c26bee91a0d0792f460270263f3c4
huebnerl <LarsHuebner2@web.de>
2018-05-28 22:18:49 +0200

    Added first draft of accelBehav calculation.

--- VisualRacing/VRMain/model/vrmetricsmanager.cpp
+++ VisualRacing/VRMain/model/vrmetricsmanager.cpp
@@ -37,5 +37,23 @@ void VRMetricsManager::updateAccelBehav()
     float maxGrip = qMax(m_data->getTireGripFL(), m_data->getTireGripFR());
     maxGrip = qMax(maxGrip, m_data->getTireGripRL());
     maxGrip = qMax(maxGrip, m_data->getTireGripRR());
+
+    // scale percentageRPM
+    float percentageRPM = m_data->getRpm() / m_data->getMaxRpm();
+    if (percentageRPM < 0.1f) {
+        percentageRPM = 0;
+        m_metrics->setAccelBehav(-1);
+        // TODO: -1?
+        return;
+    } else if (percentageRPM > 0.9f) {
+        percentageRPM = 1;
+    } else {
+        percentageRPM = 1.5625f * percentageRPM * percentageRPM - 0.3125f * percentageRPM + 0.015625f;
+    }
+
+    float accelBehav = percentageRPM + maxGrip + m_data->getThrottle();
+    accelBehav /= 3;
+
+    m_metrics->setAccelBehav(accelBehav);
 }
 
